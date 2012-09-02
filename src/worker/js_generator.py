570456228a84fbea27153f637ca3a9df9f5e8899
chris <cgrubb@ubuntu1204.(none)>
2012-09-02 16:11:58 -0500

    Moved functions to functions package. =CEG=

--- src/worker/js_generator.py
+++ src/worker/js_generator.py
@@ -8,92 +8,9 @@ import zmq
 from zmq.eventloop import zmqstream, ioloop
 ioloop.install()
 
-def gen_display(*args, **kwargs):
-    return """
-        $("#display").remove();        
-        $("#main").append($('<div id="display" />').attr('class', 'ui-widget-content'));
-        $("#display").resizable();
-        $("#display").css({
-            'margin-right':'60px',
-            'width':'30%',
-            'height':'100%',
-            'background-color':'green'
-});      
-    """
-
-#Eventually this gets more sophisticated, lookup layers, controls, etc from database
-def gen_map(*args, **kwargs):
-    return """        
-        $("#main").empty();
-        $("#main").append($('<div id="map" />'));
-        $("#map").css({
-            'float':'right',
-            'height':'100%',
-            'width':'70%'
-        });
-        $("#map").resizable();
-        index.map = {};
-        index.map.olmap = new OpenLayers.Map({
-            div: "map",
-            resolutions: [0.087890625, 0.0439453125, 0.02197265625, 0.010986328125],
-            controls: [
-                new OpenLayers.Control.Navigation(
-                    {dragPanOptions: {enableKinetic: true}}
-                )
-            ]
-        });
-        index.map.olmap.addLayer(new OpenLayers.Layer.TileCache("TileCache Layer",
-            ["http://c0.tilecache.osgeo.org/wms-c/cache/",
-             "http://c1.tilecache.osgeo.org/wms-c/cache/",
-             "http://c2.tilecache.osgeo.org/wms-c/cache/",
-             "http://c3.tilecache.osgeo.org/wms-c/cache/",
-             "http://c4.tilecache.osgeo.org/wms-c/cache/"],
-            "basic",
-            {
-                serverResolutions: [0.703125, 0.3515625, 0.17578125, 0.087890625,
-                                    0.0439453125, 0.02197265625, 0.010986328125,
-                                    0.0054931640625, 0.00274658203125, 0.001373291015625,
-                                    0.0006866455078125, 0.00034332275390625, 0.000171661376953125,
-                                    0.0000858306884765625, 0.00004291534423828125, 0.000021457672119140625],
-                buffer: 4
-            }
-        ));        
-        index.map.olmap.setCenter(new OpenLayers.LonLat(0, 0), 0);
-        index.map.getMap = function() {
-            return index.map.olmap;
-        }
-        index.map.markers = new OpenLayers.Layer.Markers("Markers");
-        index.map.olmap.addLayer(index.map.markers);
-        index.map.marker_popup = new OpenLayers.Popup();
-        index.map.olmap.addPopup(index.map.marker_popup);
-    """
-
-def plot_call(*args,**kwargs):
-    x = kwargs['x']
-    y = kwargs['y']
-    return """
-        $("#display").text("call");
-        if (index.map !== null) {  
-            var olmap = index.map.getMap();
-            olmap.removeLayer(index.map.markers);
-            var lonlat = new OpenLayers.LonLat(%s,%s);
-            olmap.panTo(lonlat);
-            index.map.markers = new OpenLayers.Layer.Markers("Markers");
-            olmap.addLayer(index.map.markers);
-            var size = new OpenLayers.Size(21,25);
-            var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
-            var icon = new OpenLayers.Icon('http://www.openlayers.org/dev/img/marker.png',size,offset);
-            index.map.markers.addMarker(new OpenLayers.Marker(lonlat,icon));
-            olmap.removePopup(index.map.marker_popup);
-            index.map.marker_popup = new OpenLayers.Popup.FramedCloud("call",
-                lonlat,
-                null,
-                "call",
-                null,
-                true);
-            olmap.addPopup(index.map.marker_popup);
-        } 
-    """%(x,y)
+from functions.display import gen_display
+from functions.map import gen_map
+from functions.call import plot_call
 
 fxn_map = {"map":gen_map,
            "plot_call":plot_call,
